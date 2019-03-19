ae03016ce4333559b315e0a41b2da899de2c6c72
Alexander Rubenov <alexander.rubenov@list.ru>
2019-03-19 19:55:27 +0300

    second

--- js/app.js
+++ js/app.js
@@ -72,60 +72,42 @@ function chooseTrajectory(fish) {
   switch(numberOfTrajectory) {
     case 1:
       startLeft = -9;
-      startTop = 12;
+      startTop = 30;
       break;
     case 2:
       startLeft = -9;
-      startTop = 26;
+      startTop = 70;
       break;
     case 3:
       startLeft = 25;
-      startTop = 46;
+      startTop = 100;
       break;
     case 4:
       startLeft = 75;
-      startTop = 46;
+      startTop = 100;
       break;
     case 5:
       startLeft = 100;
-      startTop = 12;
+      startTop = 70;
       break;
     case 6:
       startLeft = 100;
-      startTop = 26;
+      startTop = 30;
   };
 
   fish.style.left = startLeft + `vw`;
-  fish.style.top = startTop + `vw`;
+  fish.style.top = startTop + `vh`;
   
   gameField.appendChild(fish);
 
   setInterval(() => {
     fish.style.left = parseInt(fish.style.left) + getNumberSignToLeft() + `vw`;
-    fish.style.top = parseInt(fish.style.top) + getNumberSignToTop() + `vw`;
-  }, 700);
-
-  function getNumberSignToTop() {
-    if ((fish.offsetTop / gameFieldHeight * 100) <= 20) return getRandomInt(4, 8);
-    if ((fish.offsetTop / gameFieldHeight * 100) >= 80) return getRandomInt(-4, -8);
-
-    let randomNumber = getRandomInt(1, 3);
-    switch(randomNumber) {
-      case 1:
-        return getRandomInt(4, 8);
-        break;
-      case 2:
-        return getRandomInt(-4, -8);
-        break;
-      case 3:
-        return 0;
-    };
-  };
+    fish.style.top = parseInt(fish.style.top) + getNumberSignToTop() + `vh`;
+  }, 600);
 
   function getNumberSignToLeft() {
-    if ((fish.offsetLeft / gameFieldWidth * 100) <= 9) return getRandomInt(4, 8);
-    if ((fish.offsetLeft / gameFieldWidth * 100) >= 91) return getRandomInt(-4, -8);
-    console.log(`Left: ` + (fish.offsetLeft / gameFieldWidth * 100));
+    if (parseInt(fish.style.left) <= 9) return getRandomInt(4, 8);
+    if (parseInt(fish.style.left) >= 85) return getRandomInt(-4, -8);
 
     let randomNumber = getRandomInt(1, 3);
     switch(randomNumber) {
@@ -139,6 +121,24 @@ function chooseTrajectory(fish) {
         return 0;
     };
   };
+
+  function getNumberSignToTop() {
+    if (parseInt(fish.style.top) <= 20) return getRandomInt(4, 8);
+    if (parseInt(fish.style.top) >= 80) return getRandomInt(-4, -8);
+
+    let randomNumber = getRandomInt(1, 3);
+    switch(randomNumber) {
+      case 1:
+        return getRandomInt(4, 8);
+        break;
+      case 2:
+        return getRandomInt(-4, -8);
+        break;
+      case 3:
+        return 0;
+    };
+  };
+
 };
 
 function getRandomInt(min, max) {
@@ -146,14 +146,12 @@ function getRandomInt(min, max) {
 };
 
 
+function removeFishFromGameField(event) {
+  let target = event.target;
 
+  if (target.tagName !== `IMG`) return;
 
+  gameField.removeChild(target);
+}
 
-
-/*
-  * При нажатии на кнопку "Начать игру" снизу проплывает субмарина, а после исчезает медленно фон меню и появляется фон игры медленно
-  * появляются правила игры с кнопкой "понятно", после нажатия на нее запускается игра  
-
-  * при вводе имени игрока и нажатии на кнопку играть высвечиваются правила игры и кнопка "Всё понятно, поехали"
-
-*/
+gameField.addEventListener('click', removeFishFromGameField)
